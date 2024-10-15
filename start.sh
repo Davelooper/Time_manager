a9aab07d6bd20259e4730025203707181d3c9b70
Alexis <jogoldirr@gmail.com>
2024-10-15 12:49:23 +0200

    fix .sh

--- start.sh
+++ start.sh
@@ -6,8 +6,8 @@ echo "SERVICE_ENV = $SERVICE_ENV"
 # Vérifier la variable d'environnement SERVICE_ENV
 if [ "$SERVICE_ENV" = "PROD" ]; then
   echo "Lancement de l'environnement de production..."
-  docker-compose -f docker-compose.prod.yml --env-file .env.prod up -d
+  docker-compose -f docker-compose.prod.yaml --env-file .env up -d
 else
   echo "Lancement de l'environnement de développement..."
-  docker-compose -f docker-compose.dev.yml --env-file .env.dev up -d
+  docker-compose -f docker-compose.dev.yaml --env-file .env up -d
 fi
