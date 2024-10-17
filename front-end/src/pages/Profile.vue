58ebde932db768071e89924906af0d8d476fd342
Alexis <jogoldirr@gmail.com>
2024-10-17 11:15:28 +0200

    Profile page graphs

--- front-end/src/pages/Profile.vue
+++ front-end/src/pages/Profile.vue
@@ -5,11 +5,11 @@
     <h1 class="text-2xl md:text-4xl font-bold mb-4 text-white">Welcome to your profile</h1>
     <p class="text-lg md:text-xl mb-8 text-white">Here you can see your personal information</p>
 
-   <div class="bg-gradient-to-br from-slate-200/20 to-white/10 w-full min-h-10 rounded shadow p-2">
+   <div class="bg-gradient-to-br from-slate-200/20 to-white/10 w-min min-h-10 rounded shadow py-2 px-4 flex flex-col gap-1">
+     <p class="text-slate-300 font-semibold">Week</p>  
+     <p class="text-2xl bg-gradient-to-br font-bold bg-clip-text text-transparent from-yellow-300 to-yellow-600">80%</p>
       <div class="flex flex-row justify-center items-center gap-4">
-        <p class="text-white">Week</p>  
         <Progress7Days />
-        <p class="text-2xl bg-gradient-to-br font-bold bg-clip-text text-transparent from-yellow-300 to-yellow-600">80%</p>
       </div>  
     </div>
   </div>
