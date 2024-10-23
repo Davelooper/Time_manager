518fb0124be9002efd57c536571d112f42ae99b1
Alexis <jogoldirr@gmail.com>
2024-10-23 09:28:05 +0200

    Fix pipeline

--- startPipeline.bat
+++ startPipeline.bat
@@ -12,25 +12,25 @@ if "%SERVICE_ENV%" == "PROD" (
     echo Lancement de l'environnement de production...
 
     REM Arrêter et supprimer les conteneurs associés à docker-compose.prod.yaml
-    docker compose -f docker-compose.pipeline.yaml --env-file .env down
+    docker-compose -f docker-compose.pipeline.yaml --env-file .env down
 
     @REM REM Supprimer les images associées aux services de docker-compose.prod.yaml
     @REM FOR /F "tokens=*" %%i IN ('docker-compose -f docker-compose.prod.yaml config --services') DO docker rmi %%i
 
     REM Relancer et reconstruire les services pour la production
-    docker compose -f docker-compose.pipeline.yaml --env-file .env up -d --build
+    docker-compose -f docker-compose.pipeline.yaml --env-file .env up -d --build
 
 ) else (
     echo Lancement de l'environnement de développement...
 
     REM Arrêter et supprimer les conteneurs associés à docker-compose.dev.yaml
-    docker compose -f docker-compose.pipeline.yaml --env-file .env down
+    docker-compose -f docker-compose.pipeline.yaml --env-file .env down
 
     @REM REM Supprimer les images associées aux services de docker-compose.dev.yaml
     @REM FOR /F "tokens=*" %%i IN ('docker-compose -f docker-compose.dev.yaml config --services') DO docker rmi %%i
 
     REM Relancer et reconstruire les services pour le développement
-    docker compose -f docker-compose.pipeline.yaml --env-file .env up -d --build
+    docker-compose -f docker-compose.pipeline.yaml --env-file .env up -d --build
 )
 
 REM Pause pour garder la fenêtre ouverte et voir l'erreur
