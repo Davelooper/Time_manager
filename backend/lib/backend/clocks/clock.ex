9ec7a8c53312b732dfa188c3cf853e66d47dd3df
Davelooper <ddonnot@gmail.com>
2024-10-23 09:52:46 +0200

    [fix] routes create clock not put id_team in bdd and auth in working_time_controller

--- backend/lib/backend/clocks/clock.ex
+++ backend/lib/backend/clocks/clock.ex
@@ -14,7 +14,7 @@ defmodule Backend.Clocks.Clock do
   @doc false
   def changeset(clock, attrs) do
     clock
-    |> cast(attrs, [:time])
-    |> validate_required([:time])
+    |> cast(attrs, [:time, :user_id])
+    |> validate_required([:time, :user_id])
   end
 end
