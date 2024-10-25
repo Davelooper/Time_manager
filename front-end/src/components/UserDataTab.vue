bb4e076d7ddccd5ab4b1e4bf18c2e3350d91a7ba
Alexis <jogoldirr@gmail.com>
2024-10-25 08:49:26 +0200

    User update front

--- front-end/src/components/UserDataTab.vue
+++ front-end/src/components/UserDataTab.vue
@@ -1,19 +1,19 @@
 <template>
   <div>
     <!-- Votre contenu pour le premier onglet -->
-    <div class="flex flex-row gap-4 justify-center flex-wrap">
-      <div class="flex flex-col md:flex-row items-center bg-gradient-to-br backdrop-blur-lg from-slate-800/10 to-slate-500/20 rounded-lg p-2 shadow-lg">
+    <div class="flex flex-row gap-4 justify-center flex-wrap gap-y-4 mt-4">
+      <div class="w-full md:max-w-min flex items-center ">
+        <ProgressDays title="This Month" :data="[10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 90, 80, 70, 60, 50, 40, 30, 20, 10, 0, 10, 20, 30, 40, 50, 60, 70, 80]"/>
+      </div> 
+      <div class="flex-grow-[5] justify-center flex flex-col md:flex-row items-center bg-gradient-to-br backdrop-blur-lg from-slate-800/10 to-slate-500/20 rounded-lg p-2 shadow-lg">
         <BatPing />
       </div>
-      <div class="max-w-[600px] w-full flex flex-col md:flex-row items-center bg-gradient-to-br backdrop-blur-lg from-slate-800/10 to-slate-500/20 rounded-lg p-2 shadow-lg gap-6">
-        <ProgressDays title="This Month" :data="[10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 90, 80, 70, 60, 50, 40, 30, 20, 10, 0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]"/>
-      </div>
-      <div class="flex flex-col md:flex-row justify-between gap-6">
+      <div class="w-full md:max-w-min flex items-center">
         <ProgressDays title="This Week" :data="[10, 20, 30, 40, 50, 60, 70]"/>
       </div>
-      <div class="flex flex-col md:flex-row justify-between gap-6">
-        <TimeChartComponent />
-      </div>
+    </div>
+    <div class="flex flex-col md:flex-row justify-center gap-6">
+      <TimeChartComponent />  
     </div>
   </div>
 </template>
--- front-end/src/components/UserParameterTab.vue
+++ front-end/src/components/UserParameterTab.vue
@@ -1,12 +1,25 @@
 <template>
-  <Input value="valueee" label="Username" placeholder="Your username" updatable />
-  <Input value="valueee" label="Email" placeholder="Your Email" />
-  <Input value="valueee" label="Password" placeholder="Your Password" />
+  <div class="flex flex-row gap-4 pt-2">
+    <Input type="text" :value="username" label="Username" placeholder="Your username" updatable />
+    <Input type="text" :value="email" label="Email" placeholder="Your Email" />
+  </div>
+  <Input type="text" value="***********" label="Password" placeholder="Your Password" disabled/>
+  <div class="flex flex-row gap-4">
+    <Input type="select" :value="role" label="Role" disabled>
+      <option value="admin">Admin</option>
+      <option value="user">User</option>
+    </Input>
+    <Input type="select" :value="team_id" label="Team" disabled>
+      <option value="valueee">Team 1</option>
+      <option :value="team_id">Team 2</option>
+    </Input>
+  </div>
 </template>
-
 <script setup lang="ts">
+import { getDecodedToken } from '@/store/userStore';
 import Button from './Button.vue';
 import Input from './Input.vue';
 
-
-</script >
+const decodedToken = getDecodedToken(); 
+const { username, email, role, team_id } = decodedToken;
+</script > 
